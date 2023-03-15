a94b6cf8a590332992eb1b495420403b0353d172
wendysphillips <lilacinspring@gmail.com>
2023-03-15 08:25:15 -0500

    Cleaning up temporary files

--- filter_hmp_data.R
+++ filter_hmp_data.R
@@ -5,58 +5,63 @@ library(tidyverse)
 # Data tables originated from https://ibdmdb.org/tunnel/public/summary.html
 
 # The metadata table was already partially filtered from full table
-metadata <- read.delim("data/hmp2_metadata_partial.csv", sep = ",", check.names = FALSE)
+metadata_full <- read.delim("data/hmp2_metadata_partial.csv", sep = ",", check.names = FALSE)
 
 # Get rid of spaces in column names
-colnames(metadata) <- gsub(" ", "_", colnames(metadata))
+colnames(metadata_full) <- gsub(" ", "_", colnames(metadata_full))
 
 # This is the MetaPhlAn3 taxatable
-taxa <- read.delim("data/hmp_taxonomic_profiles_3.tsv", sep = "\t", check.names = FALSE)
+taxa <- read.delim("data/hmp2_taxonomic_profiles_3.tsv", sep = "\t", check.names = FALSE)
 
 # Remove superfluous part of sample names
 colnames(taxa) <- gsub("_profile", "", colnames(taxa))
 
 # Check the same samples are in metadata and taxatable
 samps <- colnames(taxa)[2:1639]
-all.equal(sort(samps), sort(metadata$`External ID`))
+all.equal(sort(samps), sort(metadata_full$`External ID`))
 
 # Remove uninformative metadata -----
 
 # Find how many unique values for each metadata column
 temp <- c()
-for (col in 1:ncol(metadata)){
-      temp <- c(temp, length(unique(metadata[,col])))
+for (col in 1:ncol(metadata_full)){
+      temp <- c(temp, length(unique(metadata_full[,col])))
 }
 
 # Filter out any metadata without any variation
-meta <- metadata[,temp > 1]
+metadata <- metadata_full[,temp > 1]
+rm(temp)
 
 # Create table with number of unique values
 #  and ratio of smallest to largest value
 temp_ratio <- c()
 temp_n <- c()
-for (col in 1:ncol(meta)) {
-      temp_min <- min(table(meta[,col]))
-      temp_max <- max(table(meta[,col]))
+for (col in 1:ncol(metadata)) {
+      temp_min <- min(table(metadata[,col]))
+      temp_max <- max(table(metadata[,col]))
       temp_ratio <- c(temp_ratio, temp_min/temp_max)
-      temp_n <- c(temp_n, nrow(table(meta[,col])))
+      temp_n <- c(temp_n, nrow(table(metadata[,col])))
+      
+      rm(temp_min, temp_max)
 }
 
-# Put the above into a datafame with meta names
-temp_df <- data.frame(meta_name = colnames(meta), n_vals = temp_n, ratio = temp_ratio)
+# Put the above into a datafame with metadata names
+temp_meta_info <- data.frame(meta_name = colnames(metadata), n_vals = temp_n, ratio = temp_ratio)
+rm(temp_n, temp_ratio)
 
 # First filter, keep things with > 2 values
 #  Of if just two values, the ratio indicates reasonable number in less abundant type
-keep <- temp_df[(temp_df$n_vals > 2) | ((temp_df$n_vals == 2) & (temp_df$ratio > 0.03)),]
-meta <- meta[,keep$meta_name]
+keep <- temp_meta_info[(temp_meta_info$n_vals > 2) | ((temp_meta_info$n_vals == 2) & (temp_meta_info$ratio > 0.03)),]
+metadata <- metadata[,keep$meta_name]
 
 # Out of curiousity, manually inspect the distribution of values in remaining columns
 #  for those columns with no more than 10 levels
-for (col in 1:ncol(meta)){
-      z <- table(meta[,col])
+for (col in 1:ncol(metadata)){
+      z <- table(metadata[,col])
       if (nrow(z) < 11) {
             print(z)
       }
+      rm(z)
 }
 
 # Taxatable manipulation ------
@@ -83,4 +88,7 @@ colSums(investigate)
 species <- species[,((colSums(species) > 98) & (colSums(species) < 102 ) )] 
 
 # Also drop those from metadata
-meta <- meta[meta$External_ID %in% colnames(species),]
+metadata <- metadata[metadata$External_ID %in% colnames(species),]
+
+# Tidy up
+rm(investigate, keep,temp_meta_info, col, samps)
