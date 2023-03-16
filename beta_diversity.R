3206622c399d7904d314d2819aaf34a378357e5c
wendysphillips <lilacinspring@gmail.com>
2023-03-16 15:54:06 -0500

    Update beta_diversity.R
    Correcting eigenvalues

--- beta_diversity.R
+++ beta_diversity.R
@@ -16,8 +16,8 @@ BC_pcoa <- ape::pcoa(BC)
 
 # Get coordinates first first two pcoa axes
 BC_pcoa_df <- data.frame(
-  pcoa1 = BC_pcoa$vectors[,1],
-  pcoa2 = BC_pcoa$vectors[,2]
+  pcoa1 = BC_pcoa$vectors[, 1],
+  pcoa2 = BC_pcoa$vectors[, 2]
 )
 
 # Simple plot
@@ -33,21 +33,28 @@ BC_pcoa_all <- left_join(BC_pcoa_df, metadata, by = "External_ID")
 
 # Plot with metadata variable
 pc_plot <- ggplot(BC_pcoa_all, aes(x = pcoa1, y = pcoa2)) +
-      geom_point(alpha = 0.5, aes(color = diagnosis)) +
-      theme_classic() +
-      labs(x = paste0("PCoA1 ", round(BC_pcoa$values$Eigenvalues[1],1), " %"), y = paste0("PCoA2 ", round(BC_pcoa$values$Eigenvalues[2],1), " %"))
+  geom_point(alpha = 0.5, aes(color = diagnosis)) +
+  theme_classic() +
+  labs(x = paste0("PCoA1 ", round(BC_pcoa$values$Eigenvalues[1], 1), " %"), y = paste0("PCoA2 ", round(BC_pcoa$values$Eigenvalues[2], 1), " %"))
 
 pc_plot
 
 # Make the above into a function
 #  to input different metadata
-pcoa_plot <- function(meta = "Chemotherapy"){
-      pc_plot <- ggplot(BC_pcoa_all, aes(x = pcoa1, y = pcoa2)) +
-            geom_point(alpha = 0.5, aes_string(color = meta)) +
-            theme_classic() +
-            labs(x = paste0("PCoA1, ", round(BC_pcoa$values$Eigenvalues[1],1), " %"), y = paste0("PCoA2, ", round(BC_pcoa$values$Eigenvalues[2],1), " %"))
-      
-      pc_plot
+pcoa_plot <- function(meta = "Chemotherapy") {
+  pc_plot <- ggplot(BC_pcoa_all, aes(x = pcoa1, y = pcoa2)) +
+    geom_point(alpha = 0.5, aes_string(color = meta)) +
+    theme_classic() +
+    labs(x = paste0("PCoA1, ", round(BC_pcoa$values$Relative_eig[1], 2), "%"), y = paste0("PCoA2, ", round(BC_pcoa$values$Relative_eig[2], 2), " %"))
+
+  pc_plot
 }
 
-pcoa_plot()
+# Plot with a categorical variable
+pcoa_plot("sex")
+
+# Plot with a continuous metadata variable
+pcoa_plot("Age_at_diagnosis")
+
+# Change legend title
+pcoa_plot("site_name") + guides(color = guide_legend("Site"))
